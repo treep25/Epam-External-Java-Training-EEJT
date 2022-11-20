22812fa077d0eb45198f260e516d9c0ddb8ad17e
treeep <bogdan.grigorenko.jb@gmail.com>
2022-11-20 15:07:22 -0100

    fix(EEJT-000A) foreach fix

--- EEJT-000A/main/java/task2/SearchBySizeChange.java
+++ EEJT-000A/main/java/task2/SearchBySizeChange.java
@@ -8,10 +8,10 @@ import java.util.stream.Collectors;
 
 public class SearchBySizeChange implements SearchByParam {
     private SearchByParam searchByParam;
-    private String getFileDir(char[]ar){
+
+    private String getFileDir(char[] ar) {
         StringBuilder stringBuilder = new StringBuilder();
-        for (char c:
-                ar) {
+        for (char c : ar) {
             if (c != ' ') {
                 stringBuilder.append(c);
             } else {
@@ -20,17 +20,20 @@ public class SearchBySizeChange implements SearchByParam {
         }
         return stringBuilder.toString();
     }
-    private boolean isFileMatches(File file , Parameters parameters){
+
+    private boolean isFileMatches(File file, Parameters parameters) {
         return file.length() >= Integer.parseInt(parameters.getSizeLess())
                 && file.length() <= Integer.parseInt(parameters.getSizeMore());
     }
-    private boolean isNextChain (SearchByParam searchByParam){
+
+    private boolean isNextChain(SearchByParam searchByParam) {
         return searchByParam != null;
     }
 
     public SearchBySizeChange(SearchByParam searchByParam) {
         this.searchByParam = searchByParam;
     }
+
     @Override
     public List<String> search(Parameters parameters, List<String> list) {
         if (list.isEmpty()) {
@@ -38,10 +41,10 @@ public class SearchBySizeChange implements SearchByParam {
             if (file.exists()) {
                 List<File> fileList = List.of(Objects.requireNonNull(file.listFiles()));
                 if (!fileList.isEmpty()) {
-                     fileList = fileList.stream().filter(x1 -> isFileMatches(x1, parameters)).collect(Collectors.toList());
+                    fileList = fileList.stream().filter(x1 -> isFileMatches(x1, parameters)).collect(Collectors.toList());
                     for (File f :
                             fileList) {
-                        list.add(dir + File.separator + f.getName() + " (" + f.length()+ ")" );
+                        list.add(dir + File.separator + f.getName() + " (" + f.length() + ")");
                     }
                 } else {
                     list.add(dir + ": " + "Данная папка не содержит файлов таких конфигураций");
@@ -51,19 +54,19 @@ public class SearchBySizeChange implements SearchByParam {
             }
         } else {
             List<String> list1 = new ArrayList<>();
-            for (String res:
+            for (String res :
                     list) {
                 File file = new File(getFileDir(res.toCharArray()));
-                if(file.exists()){
-                    if(isFileMatches(file, parameters)){
-                        list1.add(res+"("+file.length()+")");
+                if (file.exists()) {
+                    if (isFileMatches(file, parameters)) {
+                        list1.add(res + "(" + file.length() + ")");
                     }
                 }
             }
             list = list1;
         }
-        if(isNextChain(searchByParam)){
-            return searchByParam.search(parameters,list);
+        if (isNextChain(searchByParam)) {
+            return searchByParam.search(parameters, list);
         }
         return list;
     }
