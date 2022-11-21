ed39d6b6aa1185760122f286d0b54e8faf6c1f83
treeep <bogdan.grigorenko.jb@gmail.com>
2022-11-21 22:16:17 -0100

    fix(EEJT-000A) replace if clauses foreach using map , make doChain param () because it is useless

--- EEJT-000A/main/java/task2/SearchingController.java
+++ EEJT-000A/main/java/task2/SearchingController.java
@@ -1,37 +1,37 @@
 package task2;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
+import java.util.function.Function;
+import java.util.function.Predicate;
 
 public class SearchingController {
     private Parameters parameters;
-    private SearchByParam searchByParam;
+    private Map<Predicate<Parameters>, Function<SearchByParam, SearchByParam>> chainMapSearching = new LinkedHashMap<>();
+
 
     public SearchingController(Parameters parameters) {
         this.parameters = parameters;
     }
 
-    public List<String> doChain(List<String> list) {
-        if (parameters == null) {
-            if (list.isEmpty()) {
-                list = new ArrayList<>();
-            }
-            if (parameters.getSizeMore() != null
-                    && parameters.getSizeLess() != null) {
-                searchByParam = new SearchBySizeChange(searchByParam);
-            }
-            if ((parameters.getDateMore()) != null
-                    && (parameters.getDateLess()) != null) {
-                searchByParam = new SearchByDateChange(searchByParam);
-            }
-            if ((parameters.getExt()) != null) {
-                searchByParam = new SearchByExtension(searchByParam);
-            }
-            if ((parameters.getName()) != null) {
-                searchByParam = new SearchByName(searchByParam);
+    private void generateChainMapSearching() {
+        chainMapSearching.put(parameters -> parameters.getSizeMore() != null && parameters.getSizeLess() != null, SearchBySizeChange::new);
+        chainMapSearching.put(parameters -> parameters.getDateMore() != null && parameters.getDateLess() != null, SearchByDateChange::new);
+        chainMapSearching.put(parameters -> parameters.getExt() != null, SearchByExtension::new);
+        chainMapSearching.put(parameters -> parameters.getName() != null, SearchByName::new);
+    }
+
+    public List<String> doChain() {
+        List<String> paramList = new ArrayList<>();
+        SearchByParam searchByParam = null;
+        if (parameters != null) {
+            generateChainMapSearching();
+            for (Map.Entry<Predicate<Parameters>, Function<SearchByParam, SearchByParam>> entry : chainMapSearching.entrySet()) {
+                if (entry.getKey().test(parameters)) {
+                    searchByParam = entry.getValue().apply(searchByParam);
+                }
             }
             if (searchByParam != null) {
-                return searchByParam.search(parameters, list);
+                return searchByParam.search(parameters, paramList);
             }
         }
         return List.of("Вы не ввели ни одного параметра!");
