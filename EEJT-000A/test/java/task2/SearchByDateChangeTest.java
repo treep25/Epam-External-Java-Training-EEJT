50a151e07d69212730ea4adf3ad3731fa18bc2a4
treeep <bogdan.grigorenko.jb@gmail.com>
2022-11-23 18:52:13 -0100

    test(EEJT-000A)

--- EEJT-000A/test/java/task2/SearchByDateChangeTest.java
+++ EEJT-000A/test/java/task2/SearchByDateChangeTest.java
@@ -23,8 +23,8 @@ class SearchByDateChangeTest {
     void searchWhenListIsEmptyTest() {
         SearchByParam searchByDate = new SearchByDateChange(searchByParam);
         Parameters par = new Parameters(null, null, "2022-11-22 21:08:40", "2043-11-22 21:08:40", null, null);
-        List<String> list = List.of("EEJT-000A\\ANSI.txt (2022-11-22 21:08:40)", "EEJT-000A\\main (2022-11-22 21:08:40)", "EEJT-000A\\README.md (2022-11-22 21:08:40)",
-                "EEJT-000A\\test (2022-11-22 21:08:40)", "EEJT-000A\\test.txt (2022-11-22 21:08:40)", "EEJT-000A\\UTF8.txt (2022-11-22 21:08:40)");
+        List<String> list = List.of("EEJT-000A\\ANSI.txt (2022-11-22 23:42:58)", "EEJT-000A\\main (2022-11-22 23:42:58)",
+                "EEJT-000A\\test (2022-11-22 23:42:58)", "EEJT-000A\\test.txt (2022-11-22 23:42:58)", "EEJT-000A\\UTF8.txt (2022-11-22 23:42:58)");
         assertEquals(list, searchByDate.search(par, new ArrayList<>()));
     }
 
@@ -32,7 +32,7 @@ class SearchByDateChangeTest {
     void searchWhenListIsNotEmptyTest() {
         SearchByParam searchByDate = new SearchByDateChange(new SearchBySizeChange(searchByParam));
         Parameters par = new Parameters(null, null, "1356-11-22 21:08:40", "2043-11-22 21:08:40", "1", "1000");
-        List<String> list = List.of(dir + "\\test.txt (2022-11-22 21:08:40)(1)");
+        List<String> list = List.of("EEJT-000A\\test.txt (2022-11-22 23:42:58)(1)");
         assertEquals(list, searchByDate.search(par, new ArrayList<>()));
     }
 
