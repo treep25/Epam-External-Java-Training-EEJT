1d47e4131a024b16e641cf35acfc042b0dc16011
treeep <bogdan.grigorenko.jb@gmail.com>
2022-11-25 19:04:18 -0100

    feat(EEJT-000B) first steps

---
+++ EEJT-000B/main/java/entity/Product.java
@@ -0,0 +1,67 @@
+package entity;
+
+import java.util.Objects;
+
+public class Product implements Cloneable {
+    private Long id;
+    private String name;
+    private Long price;
+
+    public Product() {
+    }
+
+    public Product(Long id, String name, Long price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Product setId(Long id) {
+        this.id = id;
+        return this;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Product setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public Long getPrice() {
+        return price;
+    }
+
+    public Product setPrice(Long price) {
+        this.price = price;
+        return this;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Product)) return false;
+        Product product = (Product) o;
+        return getId().equals(product.getId()) && getName().equals(product.getName()) && getPrice().equals(product.getPrice());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getName(), getPrice());
+    }
+
+    @Override
+    public Product clone() {
+        try {
+            return (Product) super.clone();
+        } catch (CloneNotSupportedException e) {
+            throw new AssertionError();
+        }
+    }
+}
