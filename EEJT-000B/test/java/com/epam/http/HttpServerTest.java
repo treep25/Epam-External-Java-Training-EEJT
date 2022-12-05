71a7e1e87da6b805787514e056bfcff392ee0a24
treeep <bogdan.grigorenko.jb@gmail.com>
2022-12-05 13:53:53 -0100

    test(EEJT-000B) ServerHandler and Server tests

--- EEJT-000B/test/java/com/epam/http/HttpServerTest.java
+++ EEJT-000B/test/java/com/epam/http/HttpServerTest.java
@@ -1,23 +1,27 @@
 package com.epam.http;
 
-import com.epam.io.SocketIO;
 import com.epam.product.ProductRepo;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.net.Socket;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
 
 class HttpServerTest {
 
-//    @Test
-//    void run() {
-//        Socket socketMock = mock(Socket.class);
-//        ProductRepo productRepoMock = mock(ProductRepo.class);
-//        HttpServer httpServerMock = mock(HttpServer.class);
-//        HttpServerHandler httpServerHandlerMock = mock(HttpServerHandler.class);
-//        when(httpServerMock.creatThreadServerHttp(socketMock)).thenReturn(httpServerHandlerMock);
-//        verify(httpServerHandlerMock);
-//    }
+    ProductRepo productRepoMock = mock(ProductRepo.class);
+    Socket socketMock = mock(Socket.class);
+    HttpServer testObj = new HttpServer(200, productRepoMock);
+
+    @Test
+    void returnThreadOfHttpServerHandlerWhenAllParamCorrect() {
+        HttpServerHandler expected = new HttpServerHandler(socketMock, productRepoMock);
+        HttpServerHandler actual = testObj.creatThreadServerHttp(socketMock);
+
+        assertEquals(expected.getState(), actual.getState());
+
+    }
 }
