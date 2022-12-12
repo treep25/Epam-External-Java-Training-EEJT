84c832b85b53fcb7a5bbf8e5aba14415e34dd395
treeep <bogdan.grigorenko.jb@gmail.com>
2022-12-12 13:11:43 -0100

    test(EEJT-0001) making some test for Utils

---
+++ multi-project/core/src/test/java/com/epam/UtilsTest.java
@@ -0,0 +1,53 @@
+package com.epam;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UtilsTest {
+
+    @Test
+    void returnTrueWhenListOfStrAreNumbersAndMoreThenZeroTest() {
+        String[] arrayTestObjs = new String[]{"123", "123442534", "92399324"};
+
+        assertTrue(Utils.isAllPositiveNumbers(arrayTestObjs));
+    }
+
+    @Test
+    void returnFalseWhenInListOfStrOneNumberIsNotFitButOthersAreNumbersAndMoreThenZeroTest() {
+        String[] arrayTestObjs = new String[]{"123", "!@#$@%#^$^", "92399324"};
+
+        assertFalse(Utils.isAllPositiveNumbers(arrayTestObjs));
+    }
+
+    @Test
+    void returnFalseWhenListOfStrAreNotNumbers() {
+        String[] arrayTestObjs = new String[]{"!@#$@%#^$%@@1", "$#@$#@!!!", "@#@#@#$$#@@##@4"};
+
+        assertFalse(Utils.isAllPositiveNumbers(arrayTestObjs));
+    }
+
+    @Test
+    void returnFalseWhenStrIsNumberLessThenZeroTest() {
+        String[] arrayTestObjs = new String[]{"-12312", "-324342324324", "0"};
+
+        assertFalse(Utils.isAllPositiveNumbers(arrayTestObjs));
+    }
+
+    @Test
+    void returnFalseWhenStrIsSpaceTest() {
+        String[] arrayTestObjs = new String[]{"     ", "   ", ""};
+
+        assertFalse(Utils.isAllPositiveNumbers(arrayTestObjs));
+    }
+
+    @Test
+    void returnFalseWhenNullCaseTest() {
+        String[] arrayTestObjs = new String[]{};
+
+        assertFalse(Utils.isAllPositiveNumbers(arrayTestObjs));
+
+    }
+}
