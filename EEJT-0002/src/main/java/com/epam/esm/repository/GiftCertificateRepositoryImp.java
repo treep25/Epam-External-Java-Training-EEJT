07b01fa909bc7a17c781cd5225c5116f8f308f92
treeep <bogdan.grigorenko.jb@gmail.com>
2022-12-14 17:15:58 -0100

    fix(EEJT-0002) make method getAllGiftCertificates more understandable

--- EEJT-0002/src/main/java/com/epam/esm/repository/GiftCertificateRepositoryImp.java
+++ EEJT-0002/src/main/java/com/epam/esm/repository/GiftCertificateRepositoryImp.java
@@ -1,8 +1,6 @@
 package com.epam.esm.repository;
 
 import com.epam.esm.model.GiftCertificate;
-import com.epam.esm.rowMapper.GiftCertificateRowMapper;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -22,7 +20,15 @@ public class GiftCertificateRepositoryImp implements GiftCertificateRepository {
 
     @Override
     public List<GiftCertificate> getAllGiftCertificates() {
-        return jdbcTemplate.query(SqlQuery.GiftCertificate.GET_ALL_CERTIFICATES, new GiftCertificateRowMapper());
+        return jdbcTemplate.query(SqlQuery.GiftCertificate.GET_ALL_CERTIFICATES, (resultSet, i) ->
+                new GiftCertificate().setId(resultSet.getLong("id")).
+                        setName(resultSet.getString("name")).
+                        setDescription(resultSet.getString("description")).
+                        setPrice(resultSet.getInt("price")).
+                        setPrice(resultSet.getInt("price")).
+                        setDuration(resultSet.getInt("duration")).
+                        setCreateDate(resultSet.getDate("create_date")).
+                        setLastUpdateDate(resultSet.getDate("last_update_date")));
     }
 
     @Override
@@ -34,5 +40,19 @@ public class GiftCertificateRepositoryImp implements GiftCertificateRepository {
                 giftCertificate.getCreateDate(),
                 giftCertificate.getLastUpdateDate());
         return HttpStatus.CREATED;
+    }
+
+    //TODO remake this cannot find solving
+    @Override
+    public GiftCertificate getCertificateById(long id) {
+        return jdbcTemplate.queryForObject("SELECT * FROM gift_certificate WHERE id=?", (resultSet, i) ->
+                new GiftCertificate().setId(resultSet.getLong("id")).
+                        setName(resultSet.getString("name")).
+                        setDescription(resultSet.getString("description")).
+                        setPrice(resultSet.getInt("price")).
+                        setPrice(resultSet.getInt("price")).
+                        setDuration(resultSet.getInt("duration")).
+                        setCreateDate(resultSet.getDate("create_date")).
+                        setLastUpdateDate(resultSet.getDate("last_update_date")));
     }
 }
