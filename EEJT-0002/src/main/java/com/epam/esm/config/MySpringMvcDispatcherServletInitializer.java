2c16970f4abb959aa4bd28a8272b5db8dccd5431
treeep <bogdan.grigorenko.jb@gmail.com>
2022-12-16 17:35:58 -0100

    feat(EEJT-0002) some code was added

--- EEJT-0002/src/main/java/com/epam/esm/config/MySpringMvcDispatcherServletInitializer.java
+++ EEJT-0002/src/main/java/com/epam/esm/config/MySpringMvcDispatcherServletInitializer.java
@@ -5,19 +5,15 @@ import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatche
 public class MySpringMvcDispatcherServletInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
     @Override
     protected Class<?>[] getRootConfigClasses() {
-        System.out.println("getRootConfigClasses");
         return null;
     }
 
     @Override
     protected Class<?>[] getServletConfigClasses() {
-        System.out.println("getServletConfigClasses");
         return new Class[]{SpringConfig.class};
     }
-
     @Override
     protected String[] getServletMappings() {
-        System.out.println("getServletMappings");
         return new String[]{"/"};
     }
 }
