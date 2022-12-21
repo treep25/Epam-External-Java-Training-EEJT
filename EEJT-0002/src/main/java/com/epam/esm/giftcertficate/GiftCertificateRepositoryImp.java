50945ee6abdc30366f347a1ff9761e39f2a4f1eb
treeep <bogdan.grigorenko.jb@gmail.com>
2022-12-21 15:20:48 -0100

    feat(EEJT-0002) rewrite method to better testing, and remake return value type

--- EEJT-0002/src/main/java/com/epam/esm/giftcertficate/GiftCertificateRepositoryImp.java
+++ EEJT-0002/src/main/java/com/epam/esm/giftcertficate/GiftCertificateRepositoryImp.java
@@ -1,28 +1,30 @@
 package com.epam.esm.giftcertficate;
 
+import com.epam.esm.tag.Tag;
 import com.epam.esm.utils.SqlQuery;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.TransactionCallback;
+import org.springframework.transaction.support.TransactionTemplate;
 
+import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Timestamp;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
 import java.time.Instant;
-import java.util.Date;
-import java.util.List;
-import java.util.Map;
-import java.util.TimeZone;
+import java.util.*;
 
 @Repository
 public class GiftCertificateRepositoryImp implements GiftCertificateRepository {
 
     private final JdbcTemplate jdbcTemplate;
+    private final TransactionTemplate transactionTemplate;
 
     @Autowired
-    public GiftCertificateRepositoryImp(JdbcTemplate jdbcTemplate) {
+    public GiftCertificateRepositoryImp(JdbcTemplate jdbcTemplate, TransactionTemplate transactionTemplate) {
         this.jdbcTemplate = jdbcTemplate;
+        this.transactionTemplate = transactionTemplate;
     }
 
     @Override
@@ -32,7 +34,6 @@ public class GiftCertificateRepositoryImp implements GiftCertificateRepository {
                         setName(resultSet.getString("name")).
                         setDescription(resultSet.getString("description")).
                         setPrice(resultSet.getInt("price")).
-                        setPrice(resultSet.getInt("price")).
                         setDuration(resultSet.getInt("duration")).
                         setCreateDate(resultSet.getDate("create_date")).
                         setLastUpdateDate(resultSet.getDate("last_update_date")));
@@ -40,18 +41,18 @@ public class GiftCertificateRepositoryImp implements GiftCertificateRepository {
 
     @Override
     public void createCertificate(GiftCertificate giftCertificate) {
-        jdbcTemplate.update(SqlQuery.GiftCertificate.CREATE_CERTIFICATE, giftCertificate.getName(),
-                giftCertificate.getDescription(),
-                giftCertificate.getPrice(),
-                giftCertificate.getDuration(),
-                Instant.now(),
-                Instant.now());
+        transactionTemplate.execute((TransactionCallback<Object>) transactionStatus ->
+                jdbcTemplate.update(SqlQuery.GiftCertificate.CREATE_CERTIFICATE, giftCertificate.getName(),
+                        giftCertificate.getDescription(),
+                        giftCertificate.getPrice(),
+                        giftCertificate.getDuration(),
+                        Instant.now(),
+                        Instant.now()));
     }
 
-
     @Override
-    public GiftCertificate getCertificateById(long id) {
-        return jdbcTemplate.queryForObject(SqlQuery.GiftCertificate.GET_CERTIFICATE_BY_ID, new Object[]{id}, (resultSet, i) ->
+    public List<GiftCertificate> getCertificateById(long id) {
+        return jdbcTemplate.query(SqlQuery.GiftCertificate.GET_CERTIFICATE_BY_ID, new Object[]{id}, (resultSet, i) ->
                 new GiftCertificate().
                         setId(resultSet.getLong("id")).
                         setName(resultSet.getString("name")).
@@ -70,17 +71,44 @@ public class GiftCertificateRepositoryImp implements GiftCertificateRepository {
 
     @Override
     public int updateGiftCertificate(long id, Map<String, ?> updatesMap) {
-        try {
-            StringBuilder generatedQuery = new StringBuilder("UPDATE gift_certificate SET ");
-            for (Map.Entry<String, ?> entry : updatesMap.entrySet()) {
-                generatedQuery.append(entry.getKey()).append("= '").append(entry.getValue()).append("',");
-            }
-            generatedQuery.append("last_update_date = ? WHERE id = ?");
-            return jdbcTemplate.update(generatedQuery.toString(), Instant.now(), id);
-
-        } catch (IllegalArgumentException ex) {
-            throw new RuntimeException("Incorrect input");
+        List<Object> listObj = new ArrayList<>();
+        StringBuilder generatedQuery = new StringBuilder("UPDATE gift_certificate SET ");
+        for (Map.Entry<String, ?> entry : updatesMap.entrySet()) {
+            generatedQuery.append(entry.getKey()).append("= ? ,");
+            listObj.add(entry.getValue());
         }
 
+        generatedQuery.append("last_update_date = ? WHERE id = ?");
+        listObj.addAll(List.of(Instant.now(), id));
+
+        return jdbcTemplate.update(generatedQuery.toString(), listObj.toArray());
+    }
+
+    @Override
+    public long getIdByGiftCertificate(GiftCertificate giftCertificate) {
+        return jdbcTemplate.queryForObject("SELECT id FROM gift_certificate WHERE name = ? AND description = ? AND price = ? AND duration = ?",
+                new Object[]{giftCertificate.getName(), giftCertificate.getDescription(), giftCertificate.getPrice(), giftCertificate.getDuration()}
+                , Long.class);
+    }
+
+    public boolean isGiftCertificateExist(GiftCertificate giftCertificate) {
+        return Boolean.TRUE.equals(jdbcTemplate.queryForObject("SELECT EXISTS(SELECT * FROM gift_certificate WHERE name=? AND description = ? AND price = ? AND duration = ?)", Boolean.class,
+                giftCertificate.getName(), giftCertificate.getDescription(), giftCertificate.getPrice(), giftCertificate.getDuration()));
+    }
+
+    @Override
+    public void createGiftCertificateTag(long tagId, long giftCertificateId) {
+        transactionTemplate.execute((TransactionCallback<Object>) transactionStatus ->
+                jdbcTemplate.update("INSERT INTO gift_certificate_tag (gift_certificate_id,tag_id) VALUES (?,?)", giftCertificateId, tagId));
+    }
+
+    @Override
+    public int deleteGiftCertificateTag(long giftCertificateId) {
+        return jdbcTemplate.update("DELETE FROM gift_certificate_tag WHERE gift_certificate_id = ?", giftCertificateId);
+    }
+
+    @Override
+    public Optional<String> getGiftCertificateNameById(long id) {
+        return Optional.ofNullable(jdbcTemplate.queryForObject("SELECT name FROM gift_certificate WHERE id = ?", new Long[]{id}, String.class));
     }
 }
