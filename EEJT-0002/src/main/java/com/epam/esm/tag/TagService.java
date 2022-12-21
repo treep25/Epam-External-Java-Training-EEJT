bc0c219648cf9da133ade0e73756d73969eba531
treeep <bogdan.grigorenko.jb@gmail.com>
2022-12-21 15:27:39 -0100

    fix (EEJT-0002) making some checks

--- EEJT-0002/src/main/java/com/epam/esm/tag/TagService.java
+++ EEJT-0002/src/main/java/com/epam/esm/tag/TagService.java
@@ -1,6 +1,6 @@
 package com.epam.esm.tag;
 
-import com.epam.esm.giftcertficate.ItemNotFoundException;
+import com.epam.esm.utils.ItemNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -16,16 +16,24 @@ public class TagService {
     }
 
     public void createTag(Tag tag) {
-        tagRepositoryImpl.createTag(tag);
+        if (!isTagByNameExists(tag.getName())) {
+            tagRepositoryImpl.createTag(tag);
+        } else {
+            throw new IllegalArgumentException("This tag has already existed");
+        }
     }
 
     public List<Tag> getAllTags() {
-        return tagRepositoryImpl.getAllTags();
+        List<Tag> allTags = tagRepositoryImpl.getAllTags();
+        if (!allTags.isEmpty()) {
+            return allTags;
+        }
+        throw new ItemNotFoundException("There are no tags");
     }
 
-    public Tag getTagById(long id) {
-        Tag tag = tagRepositoryImpl.getTagById(id);
-        if (tag != null) {
+    public List<Tag> getTagById(long id) {
+        List<Tag> tag = tagRepositoryImpl.getTagById(id);
+        if (!tag.isEmpty()) {
             return tag;
         }
         throw new ItemNotFoundException("There are no gift certificate with id= " + id);
@@ -37,7 +45,11 @@ public class TagService {
         }
     }
 
-    public boolean getTagByName(String name) {
-        return tagRepositoryImpl.getTagByName(name);
+    public boolean isTagByNameExists(String name) {
+        return tagRepositoryImpl.isTagWithThisNameExists(name);
+    }
+
+    public long getTagIdByName(Tag tag) {
+        return tagRepositoryImpl.getIdByTag(tag);
     }
 }
