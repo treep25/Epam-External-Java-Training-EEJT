946f24371523bcfb2d7a9ddc50c35da8d1acdcd7
treeep <bogdan.grigorenko.jb@gmail.com>
2022-12-21 15:19:48 -0100

    feat(EEJT-0002) add methods to improve code reading

--- EEJT-0002/src/main/java/com/epam/esm/giftcertficate/GiftCertificateRepository.java
+++ EEJT-0002/src/main/java/com/epam/esm/giftcertficate/GiftCertificateRepository.java
@@ -1,20 +1,31 @@
 package com.epam.esm.giftcertficate;
 
 import com.epam.esm.giftcertficate.GiftCertificate;
+import com.epam.esm.tag.Tag;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
+import java.util.OptionalInt;
 
 public interface GiftCertificateRepository {
     List<GiftCertificate> getAllGiftCertificates();
 
     void createCertificate(GiftCertificate giftCertificate);
 
-    GiftCertificate getCertificateById(long id);
+    List<GiftCertificate> getCertificateById(long id);
 
     int deleteCertificate(long id);
 
     int updateGiftCertificate(long id, Map<String, ?> updatesMap);
 
+    long getIdByGiftCertificate(GiftCertificate giftCertificate);
 
+    void createGiftCertificateTag(long tagId, long giftCertificateId);
+
+    boolean isGiftCertificateExist(GiftCertificate giftCertificate);
+
+    int deleteGiftCertificateTag(long GiftCertificateId);
+
+    Optional<String> getGiftCertificateNameById(long id);
 }
