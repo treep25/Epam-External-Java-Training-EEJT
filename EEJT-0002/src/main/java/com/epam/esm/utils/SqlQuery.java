80f168cd876f90f4a3258cf18b4b393b416fb1c7
treeep <bogdan.grigorenko.jb@gmail.com>
2022-12-25 13:27:21 -0100

    feat (EEJT-0002) add class SqlQuery

--- EEJT-0002/src/main/java/com/epam/esm/utils/SqlQuery.java
+++ EEJT-0002/src/main/java/com/epam/esm/utils/SqlQuery.java
@@ -9,21 +9,50 @@ public class SqlQuery {
         public static final String GET_CERTIFICATE_BY_ID = "SELECT * FROM gift_certificate WHERE id=?";
         public static final String CREATE_CERTIFICATE = "INSERT INTO gift_certificate(name,description,price,duration,create_date,last_update_date) VALUES(?,?,?,?,?,?)";
         public static final String DELETE_CERTIFICATE = "DELETE  FROM gift_certificate WHERE id=?";
+        public static final String GET_ALL_TAGS_BY_CERTIFICATE_ID = "SELECT  t.id,t.name FROM gift_certificate_tag gct  JOIN tag t  WHERE gct.gift_certificate_id = ? AND t.id = gct.tag_id";
+        public static final String GET_ID_BY_GIFT_CERTIFICATE = "SELECT id FROM gift_certificate WHERE name = ? AND description = ? AND price = ? AND duration = ?";
+        public static final String IS_GIFT_CERTIFICATE_EXIST = "SELECT EXISTS(SELECT * FROM gift_certificate WHERE name=? AND description = ? AND price = ? AND duration = ?)";
+        public static final String CREATE_GIFT_CERTIFICATE_TAG = "INSERT INTO gift_certificate_tag (gift_certificate_id,tag_id) VALUES (?,?)";
     }
 
     public static class TagGiftCertificate {
         public static String getTypeOfSortingForDate(String method) {
             if ("DESC".equals(method.toUpperCase(Locale.ROOT))) {
-                return "SELECT t.id, t.name, gc.id, gc.name, description, price, duration, create_date, last_update_date FROM  gift_certificate_tag gct INNER JOIN tag t ON gct.tag_id = t.id INNER JOIN gift_certificate gc ON gct.gift_certificate_id = gc.id ORDER BY gc.create_date DESC";
+                return "SELECT  gc.id, gc.name, description, price, duration, create_date, last_update_date FROM  gift_certificate gc  ORDER BY gc.create_date DESC";
             }
-            return "SELECT t.id, t.name, gc.id, gc.name, description, price,duration, create_date, last_update_date FROM  gift_certificate_tag gct INNER JOIN tag t ON gct.tag_id = t.id INNER JOIN gift_certificate gc ON gct.gift_certificate_id = gc.id ORDER BY gc.create_date";
+            return "SELECT  gc.id, gc.name, description, price, duration, create_date, last_update_date FROM  gift_certificate gc  ORDER BY gc.create_date";
         }
 
-        public static String getTypeOfSortingForDateAndName(String method) {
-            if ("DESC".equals(method.toUpperCase(Locale.ROOT))) {
-                return "SELECT t.id, t.name, gc.id, gc.name, description, price, duration, create_date, last_update_date FROM  gift_certificate_tag gct INNER JOIN tag t ON gct.tag_id = t.id INNER JOIN gift_certificate gc ON gct.gift_certificate_id = gc.id ORDER BY gc.create_date AND gc.name DESC";
+        public static String getTypeOfSortingForDateAndName(String method1, String method2) {
+            if ("DESC".equals(method1.toUpperCase(Locale.ROOT)) && "ASC".equals(method2.toUpperCase(Locale.ROOT))) {
+
+                return "SELECT  gc.id, gc.name, description, price, duration, create_date, last_update_date FROM  gift_certificate gc  ORDER BY gc.name DESC , gc.create_date";
+            } else if ("ASC".equals(method1.toUpperCase(Locale.ROOT)) && "DESC".equals(method2.toUpperCase(Locale.ROOT))) {
+
+                return "SELECT  gc.id, gc.name, description, price, duration, create_date, last_update_date FROM  gift_certificate gc  ORDER BY gc.name , gc.create_date DESC";
+            } else if ("DESC".equals(method1.toUpperCase(Locale.ROOT)) && "DESC".equals(method2.toUpperCase(Locale.ROOT))) {
+
+                return "SELECT  gc.id, gc.name, description, price, duration, create_date, last_update_date FROM  gift_certificate gc  ORDER BY gc.name DESC, gc.create_date DESC";
             }
-            return "SELECT t.id, t.name, gc.id, gc.name, description, price, duration, create_date, last_update_date FROM  gift_certificate_tag gct INNER JOIN tag t ON gct.tag_id = t.id INNER JOIN gift_certificate gc ON gct.gift_certificate_id = gc.id ORDER BY gc.create_date AND gc.name";
+
+            return "SELECT  gc.id, gc.name, description, price, duration, create_date, last_update_date FROM  gift_certificate gc  ORDER BY gc.name, gc.create_date";
         }
+
+        public static final String GET_GIFT_CERTIFICATE_TAGS_BY_TAG_NAME = "SELECT   gc.id , gc.name , gc.description, gc.price, gc.duration ,gc.create_date ,gc.last_update_date FROM gift_certificate_tag gct  JOIN tag t JOIN gift_certificate gc  WHERE gc.id = gct.gift_certificate_id AND t.id = gct.tag_id AND t.name = ?";
+        public static final String GET_GIFT_CERTIFICATE_AND_TAGS_BY_NAME_OR_BY_PART_OF_NAME = "SELECT  gc.id, gc.name, description, price,duration, create_date, last_update_date FROM  gift_certificate gc WHERE gc.name LIKE ?";
+        public static final String GET_ALL_TAGS_BY_GIFT_CERTIFICATE_ID_ORDER_BY_TAG_NAME = "SELECT  t.id,t.name FROM gift_certificate_tag gct  JOIN tag t  WHERE gct.gift_certificate_id = ? AND t.id = gct.tag_id ORDER BY  t.name = ? DESC";
+        public static final String GET_ALL_TAGS_BY_GIFT_CERTIFICATE_ID = "SELECT  t.id,t.name FROM gift_certificate_tag gct  JOIN tag t  WHERE gct.gift_certificate_id = ? AND t.id = gct.tag_id";
+
+    }
+
+    public static class Tag {
+        public static final String CREATE_TAG = "INSERT INTO tag (name) VALUES(?)";
+        public static final String DELETE_TAG = "DELETE  FROM tag WHERE id=?";
+        public static final String GET_ALL_TAGS = "SELECT * FROM tag";
+        public static final String GET_TAG_BY_ID = "SELECT * FROM tag WHERE id = ?";
+        public static final String IS_TAG_WITH_NAME_EXISTS = "SELECT EXISTS(SELECT * FROM tag WHERE name=?)";
+        public static final String GET_ID_BY_TAG = "SELECT id FROM tag WHERE name = ?";
+        public static final String GET_ALL_TAGS_BY_CERTIFICATE_ID = "SELECT  t.id,t.name FROM gift_certificate_tag gct  JOIN tag t  WHERE gct.gift_certificate_id = ?  AND t.id = gct.tag_id";
+
     }
 }
