86950d7ad687893d15f8d3ec8ae8b9a26bbecd1f
treeep <bogdan.grigorenko.jb@gmail.com>
2022-12-25 13:28:54 -0100

    feat (EEJT-0002)

--- EEJT-0002/src/main/java/com/epam/esm/tag/TagRepositoryImpl.java
+++ EEJT-0002/src/main/java/com/epam/esm/tag/TagRepositoryImpl.java
@@ -1,5 +1,6 @@
 package com.epam.esm.tag;
 
+import com.epam.esm.utils.SqlQuery;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
@@ -21,33 +22,39 @@ public class TagRepositoryImpl implements TagRepository {
 
     @Override
     public void createTag(Tag tag) {
-        jdbcTemplate.update("INSERT INTO tag (name) VALUES(?)", tag.getName());
+        jdbcTemplate.update(SqlQuery.Tag.CREATE_TAG, tag.getName());
     }
 
     @Override
     public int deleteTag(long id) {
-        return jdbcTemplate.update("DELETE  FROM tag WHERE id=?", id);
+        return jdbcTemplate.update(SqlQuery.Tag.DELETE_TAG, id);
     }
 
     @Override
     public List<Tag> getAllTags() {
-        return jdbcTemplate.query("SELECT * FROM tag", (resultSet, i) ->
+        return jdbcTemplate.query(SqlQuery.Tag.GET_ALL_TAGS, (resultSet, i) ->
                 new Tag().setId(resultSet.getLong("id")).setName(resultSet.getString("name")));
     }
 
     @Override
     public List<Tag> getTagById(long id) {
-        return jdbcTemplate.query("SELECT * FROM tag WHERE id = ?", new Long[]{id}, (resultSet, i) ->
+        return jdbcTemplate.query(SqlQuery.Tag.GET_TAG_BY_ID, new Long[]{id}, (resultSet, i) ->
                 new Tag().setId(resultSet.getLong("id")).setName(resultSet.getString("name")));
     }
 
     @Override
-    public boolean isTagWithThisNameExists(String name) {
-        return Boolean.TRUE.equals(jdbcTemplate.queryForObject("SELECT EXISTS(SELECT * FROM tag WHERE name=?)", Boolean.class, name));
+    public boolean isTagWithNameExists(String name) {
+        return Boolean.TRUE.equals(jdbcTemplate.queryForObject(SqlQuery.Tag.IS_TAG_WITH_NAME_EXISTS, Boolean.class, name));
     }
 
     @Override
     public long getIdByTag(Tag tag) {
-        return jdbcTemplate.queryForObject("SELECT id FROM tag WHERE name = ?", new String[]{tag.getName()}, Long.class);
+        return jdbcTemplate.queryForObject(SqlQuery.Tag.GET_ID_BY_TAG, new String[]{tag.getName()}, Long.class);
+    }
+
+    @Override
+    public List<Tag> getAllTagsByCertificateId(long id) {
+        return jdbcTemplate.query(SqlQuery.Tag.GET_ALL_TAGS_BY_CERTIFICATE_ID, new Long[]{id}, (resultSet, i) ->
+                new Tag().setId(resultSet.getLong("id")).setName(resultSet.getString("name")));
     }
 }
