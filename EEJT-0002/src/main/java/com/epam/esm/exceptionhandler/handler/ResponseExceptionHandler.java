e2dfdbdaf659b450a1940b53b277b0f4129fd356
treeep <bogdan.grigorenko.jb@gmail.com>
2023-01-07 16:13:41 -0100

    fix (EEJT-0002) string to constants

--- EEJT-0002/src/main/java/com/epam/esm/exceptionhandler/handler/ResponseExceptionHandler.java
+++ EEJT-0002/src/main/java/com/epam/esm/exceptionhandler/handler/ResponseExceptionHandler.java
@@ -12,6 +12,7 @@ import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.sql.SQLSyntaxErrorException;
+import java.util.HashMap;
 import java.util.Map;
 
 @ControllerAdvice
@@ -23,6 +24,7 @@ public class ResponseExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(ItemNotFoundException.class)
     protected ResponseEntity<?> handleItemNotFoundException(ItemNotFoundException ex, WebRequest request) {
+
         return handleExceptionInternal(ex,
                 Map.of(HTTP_STATUS, "" + HttpStatus.NOT_FOUND, RESPONSE_BODY, Map.of(MESSAGE, ex.getMessage())),
                 new HttpHeaders(), HttpStatus.NOT_FOUND, request);
@@ -38,7 +40,7 @@ public class ResponseExceptionHandler extends ResponseEntityExceptionHandler {
     @ExceptionHandler(ApplicationException.class)
     protected ResponseEntity<?> handleApplicationException(RuntimeException ex, WebRequest request) {
         return handleExceptionInternal(ex,
-                Map.of(HTTP_STATUS, "" + HttpStatus.INTERNAL_SERVER_ERROR, "response body", Map.of(MESSAGE, MESSAGE_FOR_APPLICATION_EXCEPTION)),
+                Map.of(HTTP_STATUS, "" + HttpStatus.INTERNAL_SERVER_ERROR, RESPONSE_BODY, Map.of(MESSAGE, MESSAGE_FOR_APPLICATION_EXCEPTION)),
                 new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR, request);
     }
 }
