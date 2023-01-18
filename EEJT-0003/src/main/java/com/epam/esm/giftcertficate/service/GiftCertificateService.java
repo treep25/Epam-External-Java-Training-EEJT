5a656176371948f09b3ae5ef0f696e6a923fc417
treeep <bogdan.grigorenko.jb@gmail.com>
2023-01-18 18:13:46 -0100

    fix (EEJT-0003) remake methods on sorting

--- EEJT-0003/src/main/java/com/epam/esm/giftcertficate/service/GiftCertificateService.java
+++ EEJT-0003/src/main/java/com/epam/esm/giftcertficate/service/GiftCertificateService.java
@@ -9,15 +9,13 @@ import com.epam.esm.tag.model.Tag;
 import com.epam.esm.tag.service.TagService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.*;
+import org.springframework.hateoas.PagedModel;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
-import java.util.Locale;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 @Service
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
@@ -44,8 +42,9 @@ public class GiftCertificateService {
         throw new ServerException("This gift certificate with (name = " + giftCertificate.getName() + ") has already existed");
     }
 
-    public List<GiftCertificate> getAll() {
-        return giftCertificateRepository.findAll();
+    public Page<GiftCertificate> getAll(int page, int size) {
+        PageRequest pageRequest = PageRequest.of(page, size);
+        return Optional.of(giftCertificateRepository.findAll(pageRequest)).orElse(Page.empty(pageRequest));
     }
 
     public GiftCertificate getOneGiftCertificateById(long id) {
@@ -58,7 +57,7 @@ public class GiftCertificateService {
 
         updatesMap.remove(TAGS);
 
-        if ((!giftCertificateRepository.isGiftCertificateExistByName(updatesMap.get(NAME)))) {
+        if (!giftCertificateRepository.isGiftCertificateExistByName(updatesMap.get(NAME))) {
             GiftCertificate updatedGiftCertificate = updatingGiftCertificateUsingMapOfUpdates(updatesMap,
                     getOneGiftCertificateById(id));
 
@@ -78,6 +77,13 @@ public class GiftCertificateService {
         }
     }
 
+    public GiftCertificate updatePrice(long id, int price) {
+        GiftCertificate giftCertificate = giftCertificateRepository.findById(id).orElseThrow(
+                () -> new ServerException("There are no gift certificate with (id = " + id + ")"));
+        giftCertificate.setPrice(price);
+        return giftCertificateRepository.save(giftCertificate);
+    }
+
     public boolean deleteGiftCertificate(long id) {
         if (giftCertificateRepository.existsById(id)) {
             giftCertificateRepository.deleteById(id);
@@ -87,21 +93,41 @@ public class GiftCertificateService {
         throw new ItemNotFoundException("There are no gift certificate with (id = " + id + ")");
     }
 
-    public List<GiftCertificate> getGiftCertificatesByTagName(String tagName) {
-        return giftCertificateRepository.getAllGiftCertificateByTagName(tagName);
+    public Page<GiftCertificate> getGiftCertificatesByTagName(String tagName, int page, int size) {
+        List<GiftCertificate> allGiftCertificateByTagName = giftCertificateRepository
+                .getAllGiftCertificateByTagName(tagName, getPaginationBegin(page, size), getPaginationEnd(page, size));
+
+        return new PageImpl<>(allGiftCertificateByTagName);
     }
 
-    public List<GiftCertificate> getGiftCertificatesByNameOrByPartOfName(String partOfName) {
-        return giftCertificateRepository.getAllGiftCertificateByPartOfName(partOfName + PERCENT_FOR_SEARCHING_BY_PART);
+    public Page<GiftCertificate> getGiftCertificatesByTagsAndPrice(String firstTagName, String secondTagName, int price, int page, int size) {
+        List<GiftCertificate> giftCertificatesByTagsAndPrice = giftCertificateRepository.
+                getGiftCertificatesByTagsAndPrice(firstTagName, secondTagName, price,
+                        getPaginationBegin(page, size),
+                        getPaginationEnd(page, size));
+
+        return new PageImpl<>(giftCertificatesByTagsAndPrice);
     }
 
-    public List<GiftCertificate> getGiftCertificatesSortedByDate(String sortDirection) {
-        return giftCertificateRepository.findAll(Sort.by(Sort.Direction.valueOf(sortDirection.toUpperCase(Locale.ROOT)), CREATE_DATE));
+    public Page<GiftCertificate> getGiftCertificatesByNameOrByPartOfName(String partOfName, int page, int size) {
+        List<GiftCertificate> allGiftCertificateByPartOfName = giftCertificateRepository.
+                getAllGiftCertificateByPartOfName(partOfName + PERCENT_FOR_SEARCHING_BY_PART,
+                        getPaginationBegin(page, size),
+                        getPaginationEnd(page, size));
+
+        return new PageImpl<>(allGiftCertificateByPartOfName);
     }
 
-    public List<GiftCertificate> getGiftCertificatesSortedByDateAndByName(String firstSortDirection, String secondSortDirection) {
-        return giftCertificateRepository.findAll(Sort.by(Sort.Direction.valueOf(firstSortDirection.toUpperCase(Locale.ROOT)), NAME)
+    public Page<GiftCertificate> getGiftCertificatesSortedByDate(String sortDirection, int page, int size) {
+        PageRequest pageRequest = PageRequest.of(page, size, Sort.by(Sort.Direction.valueOf(sortDirection.toUpperCase(Locale.ROOT)), CREATE_DATE));
+        return giftCertificateRepository.findAll(pageRequest);
+    }
+
+    public Page<GiftCertificate> getGiftCertificatesSortedByDateAndByName(String firstSortDirection, String secondSortDirection, int page, int size) {
+        PageRequest pageRequest = PageRequest.of(page, size, Sort.by(Sort.Direction.valueOf(firstSortDirection.toUpperCase(Locale.ROOT)), NAME)
                 .and(Sort.by(Sort.Direction.valueOf(secondSortDirection.toUpperCase(Locale.ROOT)), CREATE_DATE)));
+
+        return giftCertificateRepository.findAll(pageRequest);
     }
 
     private GiftCertificate updatingGiftCertificateUsingMapOfUpdates(Map<String, String> updatesMap, GiftCertificate gifCertificateForUpdating) {
@@ -117,5 +143,13 @@ public class GiftCertificateService {
             }
         });
         return gifCertificateForUpdating;
+    }
+
+    private int getPaginationBegin(int page, int size) {
+        return (size * page);
+    }
+
+    private int getPaginationEnd(int page, int size) {
+        return (size * page) + size;
     }
 }
