06f3125b21c83e505b7e28a4c42aeacc4944d14c
treeep <bogdan.grigorenko.jb@gmail.com>
2023-01-19 17:02:20 -0100

    feat (EEJT-0003) write useful constants sql query

--- EEJT-0003/src/main/java/com/epam/esm/utils/query/SqlQuery.java
+++ EEJT-0003/src/main/java/com/epam/esm/utils/query/SqlQuery.java
@@ -4,56 +4,36 @@ import java.util.Locale;
 
 public class SqlQuery {
     public static class GiftCertificate {
-        public static final String GET_ALL_CERTIFICATES = "SELECT * FROM gift_certificate";
-        public static final String GET_CERTIFICATE_BY_ID = "SELECT * FROM gift_certificate WHERE id=?";
-        public static final String CREATE_CERTIFICATE = "INSERT INTO gift_certificate(name,description,price,duration,create_date,last_update_date) VALUES(?,?,?,?,?,?)";
-        public static final String DELETE_CERTIFICATE = "DELETE  FROM gift_certificate WHERE id=?";
-        public static final String GET_ALL_TAGS_BY_CERTIFICATE_ID = "SELECT  t.id,t.name FROM gift_certificate_tag gct  JOIN tag t  WHERE gct.gift_certificate_id = ? AND t.id = gct.tag_id";
-        public static final String GET_ID_BY_GIFT_CERTIFICATE = "SELECT id FROM gift_certificate WHERE name = ? AND description = ? AND price = ? AND duration = ?";
-        public static final String IS_GIFT_CERTIFICATE_EXIST = "SELECT EXISTS(SELECT * FROM gift_certificate WHERE name=?)";
-        public static final String CREATE_GIFT_CERTIFICATE_TAG = "INSERT INTO gift_certificate_tag (gift_certificate_id,tag_id) VALUES (?,?)";
-        public static final String DELETE_GIFT_CERTIFICATE_TAG = "DELETE  FROM gift_certificate_tag WHERE gift_certificate_id = ? AND tag_id = ?";
-    }
 
-    public static class TagGiftCertificate {
-        public static String getTypeOfSortingForDate(String method) {
-            if ("DESC".equals(method.toUpperCase(Locale.ROOT))) {
-                return "SELECT  gc.id, gc.name, description, price, duration, create_date, last_update_date FROM  gift_certificate gc  ORDER BY gc.create_date DESC";
-            }
-            return "SELECT  gc.id, gc.name, description, price, duration, create_date, last_update_date FROM  gift_certificate gc  ORDER BY gc.create_date";
-        }
-
-        public static String getTypeOfSortingForDateAndName(String method1, String method2) {
-            if ("DESC".equals(method1.toUpperCase(Locale.ROOT)) && "ASC".equals(method2.toUpperCase(Locale.ROOT))) {
-
-                return "SELECT  gc.id, gc.name, description, price, duration, create_date, last_update_date FROM  gift_certificate gc  ORDER BY gc.name DESC , gc.create_date";
-            } else if ("ASC".equals(method1.toUpperCase(Locale.ROOT)) && "DESC".equals(method2.toUpperCase(Locale.ROOT))) {
-
-                return "SELECT  gc.id, gc.name, description, price, duration, create_date, last_update_date FROM  gift_certificate gc  ORDER BY gc.name , gc.create_date DESC";
-            } else if ("DESC".equals(method1.toUpperCase(Locale.ROOT)) && "DESC".equals(method2.toUpperCase(Locale.ROOT))) {
-
-                return "SELECT  gc.id, gc.name, description, price, duration, create_date, last_update_date FROM  gift_certificate gc  ORDER BY gc.name DESC, gc.create_date DESC";
-            }
-
-            return "SELECT  gc.id, gc.name, description, price, duration, create_date, last_update_date FROM  gift_certificate gc  ORDER BY gc.name, gc.create_date";
-        }
-
-        public static final String GET_GIFT_CERTIFICATE_TAGS_BY_TAG_NAME = "SELECT gc.id , gc.name , gc.description, gc.price, gc.duration ,gc.create_date ,gc.last_update_date FROM gift_certificate_tag gct  JOIN tag t JOIN gift_certificate gc  WHERE gc.id = gct.gift_certificate_id AND t.id = gct.tag_id AND t.name = ?";
-        public static final String GET_GIFT_CERTIFICATE_AND_TAGS_BY_NAME_OR_BY_PART_OF_NAME = "SELECT  gc.id, gc.name, description, price,duration, create_date, last_update_date FROM  gift_certificate gc WHERE gc.name LIKE ?";
-        public static final String GET_ALL_TAGS_BY_GIFT_CERTIFICATE_ID_ORDER_BY_TAG_NAME = "SELECT  t.id,t.name FROM gift_certificate_tag gct  JOIN tag t  WHERE gct.gift_certificate_id = ? AND t.id = gct.tag_id ORDER BY  t.name = ? DESC";
-        public static final String GET_ALL_TAGS_BY_GIFT_CERTIFICATE_ID = "SELECT  t.id,t.name FROM gift_certificate_tag gct  JOIN tag t  WHERE gct.gift_certificate_id = ? AND t.id = gct.tag_id";
-
-
+        public static final String IS_GIFT_CERTIFICATE_EXISTS_BY_NAME = "select case when count(gc)> 0 then true else false end from GiftCertificate gc where gc.name = :name";
+        public static final String GET_ALL_GIFT_CERTIFICATES_BY_TAG_NAME = "SELECT gc.id , gc.name , gc.description, gc.price, gc.duration ,gc.create_date ,gc.last_update_date FROM gift_certificate_tags gct  JOIN tag t JOIN gift_certificate gc  WHERE gc.id = gct.gift_certificate_id AND t.id = gct.tags_id AND t.name = ?1 LIMIT ?2,?3";
+        public static final String GET_ALL_GIFT_CERTIFICATES_BY_NAME_OR_PART_OF_NAME = "SELECT * FROM  gift_certificate gc WHERE gc.name LIKE ?1 LIMIT ?2,?3";
+        public static final String GET_ALL_GIFT_CERTIFICATES_BY_TAGS_AND_PRICE = "SELECT DISTINCT (gc.id) , gc.name , gc.description, gc.price, gc.duration ,gc.create_date ,gc.last_update_date FROM gift_certificate_tags gct  JOIN tag t JOIN gift_certificate gc  WHERE gc.id = gct.gift_certificate_id AND t.id = gct.tags_id AND (t.name = ?1 OR t.name =?2) AND gc.price >= ?3 LIMIT ?4,?5";
     }
 
     public static class Tag {
-        public static final String CREATE_TAG = "INSERT INTO tag (name) VALUES(?)";
-        public static final String DELETE_TAG = "DELETE  FROM tag WHERE id=?";
-        public static final String GET_ALL_TAGS = "SELECT * FROM tag";
-        public static final String GET_TAG_BY_ID = "SELECT * FROM tag WHERE id = ?";
-        public static final String IS_TAG_EXISTS_BY_NAME = "SELECT EXISTS(SELECT * FROM tag WHERE name = ?1)";
-        public static final String GET_ID_BY_TAG = "SELECT id FROM tag WHERE name = ?";
-        public static final String GET_ALL_TAGS_BY_CERTIFICATE_ID = "SELECT  t.id,t.name FROM gift_certificate_tag gct  JOIN tag t  WHERE gct.gift_certificate_id = ?  AND t.id = gct.tag_id";
+
+        public static final String GET_ID_BY_TAG_NAME = "SELECT t.id FROM Tag t WHERE t.name = :name";
+        public static final String IS_TAG_EXISTS_BY_NAME = "select case when count(t)> 0 then true else false end from Tag t where t.name = :name";
+        public static final String GET_THE_MOST_WIDELY_USED_TAG_WITH_THE_HIGHEST_COST_OF_ALL_ORDERS = "SELECT *\n" +
+                "FROM (SELECT t.id, t.name\n" +
+                "      FROM orders o\n" +
+                "               INNER JOIN user_orders uo on uo.orders_id = o.id AND uo.user_id = (SELECT user.id\n" +
+                "                                                                                  FROM (SELECT res.id, res.sumOfAllUsersOrders\n" +
+                "                                                                                        FROM (SELECT u.id, SUM(o.cost) as sumOfAllUsersOrders\n" +
+                "                                                                                              FROM orders o,\n" +
+                "                                                                                                   user u,\n" +
+                "                                                                                                   user_orders uo\n" +
+                "                                                                                              WHERE o.id = uo.orders_id\n" +
+                "                                                                                                AND u.id = uo.user_id\n" +
+                "                                                                                              GROUP BY u.id\n" +
+                "                                                                                              ORDER BY sumOfAllUsersOrders DESC) res\n" +
+                "                                                                                        having sumOfAllUsersOrders = MAX(sumOfAllUsersOrders)) user)\n" +
+                "               INNER JOIN gift_certificate_tags gct on o.gift_certificate_id = gct.gift_certificate_id\n" +
+                "               INNER JOIN tag t on t.id = gct.tags_id) t\n" +
+                "GROUP BY t.name\n" +
+                "ORDER BY COUNT(t.name) DESC\n" +
+                "LIMIT 1;";
 
     }
 }
