6de7634cfcf0f3616b1bea5e0f8db77d0598d0d4
treeep <bogdan.grigorenko.jb@gmail.com>
2023-01-19 17:02:43 -0100

    fix (EEJT-0003) rewrite with constants sql query

--- EEJT-0003/src/main/java/com/epam/esm/giftcertficate/repository/GiftCertificateRepository.java
+++ EEJT-0003/src/main/java/com/epam/esm/giftcertficate/repository/GiftCertificateRepository.java
@@ -1,26 +1,26 @@
 package com.epam.esm.giftcertficate.repository;
 
 import com.epam.esm.giftcertficate.model.GiftCertificate;
+import com.epam.esm.utils.query.SqlQuery;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Set;
 
 @Repository
 public interface GiftCertificateRepository extends JpaRepository<GiftCertificate, Long> {
 
-    @Query("select case when count(gc)> 0 then true else false end from GiftCertificate gc where gc.name = :name")
+    @Query(SqlQuery.GiftCertificate.IS_GIFT_CERTIFICATE_EXISTS_BY_NAME)
     boolean isGiftCertificateExistByName(@Param("name") String name);
 
-    @Query(value = "SELECT gc.id , gc.name , gc.description, gc.price, gc.duration ,gc.create_date ,gc.last_update_date FROM gift_certificate_tags gct  JOIN tag t JOIN gift_certificate gc  WHERE gc.id = gct.gift_certificate_id AND t.id = gct.tags_id AND t.name = ?1 LIMIT ?2,?3", nativeQuery = true)
-    List<GiftCertificate> getAllGiftCertificateByTagName(String tagName, int begin, int end);
+    @Query(value = SqlQuery.GiftCertificate.GET_ALL_GIFT_CERTIFICATES_BY_TAG_NAME, nativeQuery = true)
+    List<GiftCertificate> getAllGiftCertificatesByTagName(String tagName, int begin, int end);
 
-    @Query(value = "SELECT * FROM  gift_certificate gc WHERE gc.name LIKE ?1 LIMIT ?2,?3", nativeQuery = true)
+    @Query(value = SqlQuery.GiftCertificate.GET_ALL_GIFT_CERTIFICATES_BY_NAME_OR_PART_OF_NAME, nativeQuery = true)
     List<GiftCertificate> getAllGiftCertificateByPartOfName(String partOfName, int begin, int end);
 
-    @Query(value = "SELECT DISTINCT (gc.id) , gc.name , gc.description, gc.price, gc.duration ,gc.create_date ,gc.last_update_date FROM gift_certificate_tags gct  JOIN tag t JOIN gift_certificate gc  WHERE gc.id = gct.gift_certificate_id AND t.id = gct.tags_id AND (t.name = ?1 OR t.name =?2) AND gc.price >= ?3 LIMIT ?4,?5", nativeQuery = true)
+    @Query(value = SqlQuery.GiftCertificate.GET_ALL_GIFT_CERTIFICATES_BY_TAGS_AND_PRICE, nativeQuery = true)
     List<GiftCertificate> getGiftCertificatesByTagsAndPrice(String tagName1, String tagName2, int price, int begin, int end);
 }
--- EEJT-0003/src/main/java/com/epam/esm/tag/repository/TagRepository.java
+++ EEJT-0003/src/main/java/com/epam/esm/tag/repository/TagRepository.java
@@ -12,10 +12,10 @@ import java.util.List;
 
 @Repository
 public interface TagRepository extends JpaRepository<Tag, Long> {
-    @Query("select case when count(t)> 0 then true else false end from Tag t where t.name = :name")
+    @Query(SqlQuery.Tag.IS_TAG_EXISTS_BY_NAME)
     boolean isTagExistsByName(@Param("name") String tagName);
 
-    @Query("SELECT t.id FROM Tag t WHERE t.name = :name")
+    @Query(SqlQuery.Tag.GET_ID_BY_TAG_NAME)
     long getIdByTagName(@Param("name") String tagName);
 
     @Query(value = SqlQuery.Tag.GET_THE_MOST_WIDELY_USED_TAG_WITH_THE_HIGHEST_COST_OF_ALL_ORDERS, nativeQuery = true)
