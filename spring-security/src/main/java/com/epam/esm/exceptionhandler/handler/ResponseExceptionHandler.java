01c58fde9504ba46fbf00f9afffa5a96aa4f04c5
treeep <bogdan.grigorenko.jb@gmail.com>
2023-02-10 18:33:05 -0100

    feat (EEJT-0004) refactoring

--- spring-security/src/main/java/com/epam/esm/exceptionhandler/handler/ResponseExceptionHandler.java
+++ spring-security/src/main/java/com/epam/esm/exceptionhandler/handler/ResponseExceptionHandler.java
@@ -9,7 +9,6 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.AccessDeniedException;
-import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
