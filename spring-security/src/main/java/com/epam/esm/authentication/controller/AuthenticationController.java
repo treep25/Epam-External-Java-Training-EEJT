6f0a77279b6c726b7bf4b6ec81de103a5cbcbb71
treeep <bogdan.grigorenko.jb@gmail.com>
2023-02-13 17:42:57 -0100

    feat (EEJT-0004) add validation

--- spring-security/src/main/java/com/epam/esm/authentication/controller/AuthenticationController.java
+++ spring-security/src/main/java/com/epam/esm/authentication/controller/AuthenticationController.java
@@ -2,15 +2,14 @@ package com.epam.esm.authentication.controller;
 
 import com.epam.esm.authentication.model.AuthenticationRefreshRequest;
 import com.epam.esm.authentication.model.AuthenticationRequest;
-import com.epam.esm.authentication.model.AuthenticationResponse;
+import com.epam.esm.authentication.model.GoogleRequestToken;
 import com.epam.esm.authentication.model.RegisterRequest;
 import com.epam.esm.authentication.service.AuthenticationService;
-import com.epam.esm.user.model.User;
+import com.epam.esm.exceptionhandler.exception.ServerException;
+import com.epam.esm.utils.validation.DataValidation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,18 +23,39 @@ public class AuthenticationController {
     private final AuthenticationService service;
 
     @PostMapping("/register")
-    public ResponseEntity<AuthenticationResponse> register(@RequestBody RegisterRequest request) {
-        return ResponseEntity.ok(service.register(request));
+    public ResponseEntity<?> register(@RequestBody RegisterRequest request) {
+        if(DataValidation.validateRegisterRequest(request)){
+
+            return ResponseEntity.ok(service.register(request));
+        }
+        throw new ServerException("username or password cannot be empty");
     }
 
     @PostMapping("/login")
-    public ResponseEntity<AuthenticationResponse> authenticate(@RequestBody AuthenticationRequest request) {
-        return ResponseEntity.ok(service.authenticate(request));
+    public ResponseEntity<?> authenticate(@RequestBody AuthenticationRequest request) {
+        if(DataValidation.validateAuthenticationRequest(request)){
+
+            return ResponseEntity.ok(service.authenticate(request));
+        }
+        throw new ServerException("username or password cannot be empty");
     }
 
     @PostMapping("/refresh")
-    public ResponseEntity<AuthenticationResponse> refresh(@RequestBody AuthenticationRefreshRequest token) {
-        return ResponseEntity.ok(service.refreshToken(token.getRefreshToken()));
+    public ResponseEntity<?> refresh(@RequestBody AuthenticationRefreshRequest token) {
+        if(DataValidation.isStringValid(token.getRefreshToken())){
+
+            return ResponseEntity.ok(service.refreshToken(token));
+        }
+        throw new ServerException("refresh token cannot be empty or be consists of only numbers");
+    }
+
+    @PostMapping("/google-auth")
+    public ResponseEntity<?> loginWithGoogle(@RequestBody GoogleRequestToken token) {
+        if(DataValidation.isStringValid(token.getGoogleToken())){
+
+            return ResponseEntity.ok(service.authenticate(token));
+        }
+        throw new ServerException("google token_id cannot be empty or be consists of only numbers");
     }
 
 }
--- spring-security/src/main/java/com/epam/esm/authentication/service/AuthenticationService.java
+++ spring-security/src/main/java/com/epam/esm/authentication/service/AuthenticationService.java
@@ -1,14 +1,15 @@
 package com.epam.esm.authentication.service;
 
 
-import com.epam.esm.authentication.model.AuthenticationRequest;
-import com.epam.esm.authentication.model.AuthenticationResponse;
-import com.epam.esm.authentication.model.RegisterRequest;
+import com.epam.esm.authentication.model.*;
+import com.epam.esm.exceptionhandler.exception.ServerException;
 import com.epam.esm.exceptionhandler.exception.UserInvalidData;
+import com.epam.esm.jwt.google.GoogleJwtService;
 import com.epam.esm.role.Role;
 import com.epam.esm.jwt.service.JwtService;
 import com.epam.esm.user.model.User;
 import com.epam.esm.user.repository.UserRepository;
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.JwtException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.AccessDeniedException;
@@ -17,22 +18,44 @@ import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
+@EnableTransactionManagement
 public class AuthenticationService {
     private final UserRepository repository;
     private final PasswordEncoder passwordEncoder;
     private final JwtService jwtService;
+    private final GoogleJwtService googleJwtService;
     private final AuthenticationManager authenticationManager;
 
+    @Transactional
     public AuthenticationResponse register(RegisterRequest request) {
-        User user = User.builder()
-                .name(request.getUsername())
-                .password(passwordEncoder.encode(request.getPassword()))
-                .role(Role.USER)
-                .build();
+        User user = new User();
+
+        if (!repository.existsByName(request.getUsername())) {
+            user = User.builder()
+                    .name(request.getUsername())
+                    .password(passwordEncoder.encode(request.getPassword()))
+                    .role(Role.USER)
+                    .build();
+        }
+        Optional<User> userByName = repository.findByName(request.getUsername());
+        if (userByName.isPresent()) {
+            if (userByName.get().getPassword() == null) {
+                user = userByName.get();
+                user.setPassword(passwordEncoder.encode(request.getPassword()));
+            } else {
+                throw new ServerException("Sorry, such username has already taken");
+            }
+        }
+
         repository.save(user);
+
         String accessToken = jwtService.generateToken(user);
         String refreshToken = jwtService.generateRefreshToken(user);
         return AuthenticationResponse.builder()
@@ -40,7 +63,7 @@ public class AuthenticationService {
                 .refreshToken(refreshToken)
                 .build();
     }
-
+    @Transactional
     public AuthenticationResponse authenticate(AuthenticationRequest request) {
         User user = repository.findByName(request.getUsername())
                 .orElseThrow(() -> new AccessDeniedException("incorrect login or password"));
@@ -63,12 +86,11 @@ public class AuthenticationService {
                 .build();
     }
 
-    public AuthenticationResponse refreshToken(String token) {
-
-        User currentUser = repository.findByName(jwtService.extractUsername(token))
+    public AuthenticationResponse refreshToken(AuthenticationRefreshRequest request) {
+        User currentUser = repository.findByName(jwtService.extractUsername(request.getRefreshToken()))
                 .orElseThrow(() -> new UserInvalidData("error occurred during the request"));
 
-        if (jwtService.isTokenValid(token, currentUser)) {
+        if (jwtService.isTokenValid(request.getRefreshToken(), currentUser)) {
 
             String accessToken = jwtService.generateToken(currentUser);
             String refreshToken = jwtService.generateRefreshToken(currentUser);
@@ -78,6 +100,28 @@ public class AuthenticationService {
                     .build();
         }
         throw new AccessDeniedException("Token is not valid");
+    }
+
+    @Transactional
+    public AuthenticationResponse authenticate(GoogleRequestToken request) {
+        if (googleJwtService.isTokenValid(request.getGoogleToken())) {
+            String username = googleJwtService.extractUsername(request.getGoogleToken());
+
+            if (username != null) {
+                Optional<User> userByName = repository.findByName(username);
+                if (userByName.isEmpty()) {
+                    userByName = Optional.of(repository.save(User.builder().name(username.trim()).role(Role.USER).build()));
+                }
+
+                String accessToken = jwtService.generateToken(userByName.get());
+                String refreshToken = jwtService.generateRefreshToken(userByName.get());
+
+                return AuthenticationResponse.builder().accessToken(accessToken).refreshToken(refreshToken).build();
+            }
+            throw new AccessDeniedException("Bad token signature");
+        }
+        throw new AccessDeniedException("Token is not valid");
+
     }
 }
 
