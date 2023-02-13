0cd5b45a8f42996ce7cabe324acb451cecce6140
treeep <bogdan.grigorenko.jb@gmail.com>
2023-02-13 18:54:07 -0100

    feat (EEJT-0004) exception handling (global)

---
+++ spring-security/src/main/java/com/epam/esm/exceptionhandler/ExceptionController.java
@@ -0,0 +1,23 @@
+package com.epam.esm.exceptionhandler;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.nio.file.AccessDeniedException;
+import java.util.Map;
+
+@RestController
+public class ExceptionController implements ErrorController {
+
+    @RequestMapping("/error")
+    public ResponseEntity<?> handleError(final HttpServletRequest request,
+                                         final HttpServletResponse response) {
+        return new ResponseEntity<>(Map.of("HTTP Status", "" + HttpStatus.FORBIDDEN, "response body", Map.of("message", "invalid token detected")),HttpStatus.FORBIDDEN);
+    }
+
+}
