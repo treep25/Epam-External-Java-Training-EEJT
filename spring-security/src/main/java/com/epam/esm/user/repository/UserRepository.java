77f9bfdf89a84fc23a1d0f05a25156caee2e99ce
treeep <bogdan.grigorenko.jb@gmail.com>
2023-02-13 17:41:48 -0100

    feat (EEJT-0004) add existsByName()

--- spring-security/src/main/java/com/epam/esm/user/repository/UserRepository.java
+++ spring-security/src/main/java/com/epam/esm/user/repository/UserRepository.java
@@ -2,13 +2,12 @@ package com.epam.esm.user.repository;
 
 import com.epam.esm.user.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-
+    boolean existsByName(String username);
     Optional<User> findByName(String name);
 }
