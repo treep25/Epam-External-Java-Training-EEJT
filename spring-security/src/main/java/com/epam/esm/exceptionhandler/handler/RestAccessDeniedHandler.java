489045e7c5fdab14efaa4fcaec3f5a488cddb411
treeep <bogdan.grigorenko.jb@gmail.com>
2023-02-16 20:01:39 -0100

    feat (EEJT-0004) improved error handling

---
+++ spring-security/src/main/java/com/epam/esm/exceptionhandler/handler/RestAccessDeniedHandler.java
@@ -0,0 +1,27 @@
+package com.epam.esm.exceptionhandler.handler;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerExceptionResolver;
+
+import java.io.IOException;
+
+@Component("restAccessDeniedHandler")
+public class RestAccessDeniedHandler implements AccessDeniedHandler {
+
+    private final HandlerExceptionResolver resolver;
+
+    public RestAccessDeniedHandler(@Qualifier("handlerExceptionResolver") HandlerExceptionResolver resolver) {
+        this.resolver = resolver;
+    }
+
+    @Override
+    public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) {
+        resolver.resolveException(request, response, null, accessDeniedException);
+    }
+}
