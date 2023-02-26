97c060e146aae0f64fc947521ce90d6ba84e5ec8
treeep <bogdan.grigorenko.jb@gmail.com>
2023-02-26 12:13:46 -0100

    fix (EEJT-0004) rename class and add method in User Entity is() , witch verifies role

--- spring-security/src/main/java/com/epam/esm/user/authComponent/AuthUserVerify.java
+++ spring-security/src/main/java/com/epam/esm/user/authComponent/AuthUserVerify.java
@@ -1,4 +1,4 @@
-package com.epam.esm.orders.authComponent;
+package com.epam.esm.user.authComponent;
 
 import com.epam.esm.exceptionhandler.exception.ServerException;
 import com.epam.esm.role.Role;
@@ -9,18 +9,13 @@ import org.springframework.stereotype.Component;
 
 @Component
 @Slf4j
-public class AuthOrderComponent {
-
+public class AuthUserVerify {
     public boolean hasPermission(User user, Long id) {
-        log.debug("Validation request model Order ID " + id);
-
+        log.debug("Validation request model User ID " + id);
         if (DataValidation.moreThenZero(id)) {
-            if(user.getOrders() !=null){
-                return user.getOrdersIds().contains(id) || user.getRole().equals(Role.ADMIN);
-            }
-            return user.getRole().equals(Role.ADMIN);
+            return id == user.getId() || user.is(Role.ADMIN);
         }
-        log.error("The order ID is not valid: id = " + id);
+        log.error("The user ID is not valid: id = " + id);
         throw new ServerException("value id = " + id + " should be more than zero");
     }
 }
