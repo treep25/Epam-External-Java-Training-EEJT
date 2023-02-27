19683a6ebd5a91fc573583ed7b16ab64cf26cb03
treeep <bogdan.grigorenko.jb@gmail.com>
2023-02-27 16:01:39 -0100

    feat (EEJT-0004) add mail support for registration v2

---
+++ spring-security/src/main/java/com/epam/esm/confirmation_token/repository/ConfirmationTokenRepository.java
@@ -0,0 +1,10 @@
+package com.epam.esm.confirmation_token.repository;
+
+import com.epam.esm.confirmation_token.model.ConfirmationToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ConfirmationTokenRepository extends JpaRepository<ConfirmationToken, Long> {
+    ConfirmationToken findByConfirmationToken(String confirmationToken);
+}
