55a2b8132996bcfa065b2cb29712b5ada92cb6e5
treeep <bogdan.grigorenko.jb@gmail.com>
2023-03-02 15:43:26 -0100

    fix (EEJT-0004) refactoring

--- spring-security/src/test/java/com/epam/esm/tag/controller/TagControllerTest.java
+++ spring-security/src/test/java/com/epam/esm/tag/controller/TagControllerTest.java
@@ -6,7 +6,6 @@ import com.epam.esm.tag.repository.TagRepository;
 import com.epam.esm.user.model.User;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.SneakyThrows;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
@@ -16,7 +15,6 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.context.WebApplicationContext;
 
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
