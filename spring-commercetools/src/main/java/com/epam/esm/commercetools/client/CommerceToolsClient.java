be39fc02f3b41f760432e48fb3ac374da9c50ef3
treeep <bogdan.grigorenko.jb@gmail.com>
2023-03-31 20:21:18 +0000

    fix(EEJT-0007) replace Env with @Value

--- spring-commercetools/src/main/java/com/epam/esm/commercetools/client/CommerceToolsClient.java
+++ spring-commercetools/src/main/java/com/epam/esm/commercetools/client/CommerceToolsClient.java
@@ -5,29 +5,37 @@ import com.commercetools.api.defaultconfig.ApiRootBuilder;
 import com.commercetools.api.defaultconfig.ServiceRegion;
 import io.vrap.rmf.base.client.oauth2.ClientCredentials;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.Environment;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
+@PropertySource("classpath:application.yaml")
 public class CommerceToolsClient {
 
-    private final Environment environment;
+    private final String clientId;
+    private final String clientSecret;
+    private final String projectKey;
 
     @Autowired
-    public CommerceToolsClient(Environment environment) {
-        this.environment = environment;
+    public CommerceToolsClient(@Value("${commerce.clientId}") String clientId,
+                               @Value("${commerce.clientSecret}") String clientSecret,
+                               @Value("${commerce.projectKey}") String projectKey) {
+        this.clientId = clientId;
+        this.clientSecret = clientSecret;
+        this.projectKey = projectKey;
     }
 
     @Bean
     public ProjectApiRoot projectApiRoot() {
-
+        System.out.println(clientId);
         return ApiRootBuilder.of()
                 .defaultClient(ClientCredentials.of()
-                                .withClientId(environment.getProperty("commerce.clientId"))
-                                .withClientSecret(environment.getProperty("commerce.clientSecret"))
+                                .withClientId(clientId)
+                                .withClientSecret(clientSecret)
                                 .build(),
                         ServiceRegion.GCP_EUROPE_WEST1)
-                .build("spring-commersetools");
+                .build(projectKey);
     }
 }
