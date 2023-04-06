51a3d58f14f1bc287c800dc03983b91ef4880af3
treeep <bogdan.grigorenko.jb@gmail.com>
2023-04-06 17:03:38 +0000

    feat(EEJT-0007) split logic with searching by name and by tag one

--- spring-commercetools/src/main/java/com/epam/esm/commercetools/service/GiftCertificateCommerceService.java
+++ spring-commercetools/src/main/java/com/epam/esm/commercetools/service/GiftCertificateCommerceService.java
@@ -76,9 +76,9 @@ public class GiftCertificateCommerceService {
         return giftCertificateCommerceRepository.findByName(name);
     }
 
-    public List<CommerceGiftCertificate> findByTagName(String name, PageRequest pageRequest) {
+    public Page<CommerceGiftCertificate> findByTagName(String name, PageRequest pageRequest) {
         log.info("Service receives params for searching by tag name {}", name);
 
-        return giftCertificateCommerceRepository.findByTagName(name, pageRequest);
+        return new PageImpl<>(giftCertificateCommerceRepository.findByTagName(name, pageRequest));
     }
 }
